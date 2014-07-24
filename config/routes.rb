7afe7c7393d4cce3b15f7c41669c720243c541dc
Maurizio Mangione <granzebru@gmail.com>
2014-07-24 16:28:14 +0200

    basic routes and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
+  get 'create/index'
+
+  get 'list/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'list#index'
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  get 'create' => 'create#index'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
